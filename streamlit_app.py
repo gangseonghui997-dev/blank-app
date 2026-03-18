2d417f1cc4e06c45a2a89a5f83b36b7573298fb5
gangseonghui997-dev <gangseonghui997@gmail.com>
2026-03-18 11:56:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈픽셀 디스패치")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
