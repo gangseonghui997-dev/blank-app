a07efd0e6daf61b2497fba365e93d9f8e73e92f3
gangseonghui997-dev <gangseonghui997@gmail.com>
2026-03-18 11:59:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("🎈픽셀 디스패치")
+st.header("AI 기반 이미지 생성 및 편집")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
